e81a3ee823e696dde3b44de522e635e1acba9d8f
becky07110711 <becky0711@gmail.com>
2020-04-01 17:53:44 +0800

    new project

---
+++ class02/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  class02
+//
+//  Created by becky on 2020/4/1.
+//  Copyright © 2020 becky. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
